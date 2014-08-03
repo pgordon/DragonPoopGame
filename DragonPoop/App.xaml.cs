7b61e0e4a0365590028b03fc34b15789788fa84c
supererogatory <mrs.mattstone@gmail.com>
2014-08-02 23:31:40 -0700

    new project, repository
    Visual Studio generated application WPF Application

---
+++ DragonPoop/App.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace DragonPoop
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// http://www.wpf-tutorial.com/wpf-application/working-with-app-xaml/
+    ///  Define global resources, subscribe to events
+    /// </summary>
+    public partial class App : Application
+    {
+
+
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            
+        }
+    }
+}
