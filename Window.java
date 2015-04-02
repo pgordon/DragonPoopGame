a281180900836a9c4f7553d2e0ebe22aca06b2e9
pgordon <mrs.mattstone@gmail.com>
2015-04-01 22:14:36 -0700

    java prototype version
    based on www.gametutorial.net specifically the moon lander game. Still
    mostly rocket landing on the moon. Some tweaks to fonts and rocket
    controls: now can move in all 4 directions and spacebar shoots the
    flames.

---
+++ Window.java
@@ -0,0 +1,56 @@
+package DragonPoopGame;
+
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+
+/**
+ * Creates frame and set its properties.
+ *
+ * @author www.gametutorial.net
+ * Edited by Piper Gordon
+ */
+
+public class Window extends JFrame{
+
+    private Window()
+    {
+        // Sets the title for this frame.
+        this.setTitle("Dragon Poop");
+
+        // Sets size of the frame.
+        if(false) // Full screen mode
+        {
+            // Disables decorations for this frame.
+            this.setUndecorated(true);
+            // Puts the frame to full screen.
+            this.setExtendedState(this.MAXIMIZED_BOTH);
+        }
+        else // Window mode
+        {
+            // Size of the frame.
+            this.setSize(800, 600);
+            // Puts frame to center of the screen.
+            this.setLocationRelativeTo(null);
+            // So that frame cannot be resizable by the user.
+            this.setResizable(false);
+        }
+
+        // Exit the application when the user closes frame.
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        this.setContentPane(new DragonPoopGame.Framework());
+
+        this.setVisible(true);
+    }
+
+    public static void main(String[] args)
+    {
+        // Use the event dispatch thread to build the UI for thread-safety.
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new Window();
+            }
+        });
+    }
+}
